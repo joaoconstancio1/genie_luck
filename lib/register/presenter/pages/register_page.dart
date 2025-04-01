8273117b2aaaabab28d923cd7d4695a26b51ed28
joao vitor <joaovitorconstancio@hotmail.com>
2025-04-01 10:37:41 -0300

    blocBuilder implements

--- lib/register/presenter/pages/register_page.dart
+++ lib/register/presenter/pages/register_page.dart
@@ -7,7 +7,6 @@ import 'package:genie_luck/core/utils/validators.dart';
 import 'package:genie_luck/register/presenter/cubit/register_cubit.dart';
 import 'package:genie_luck/register/presenter/cubit/register_states.dart';
 import 'package:get_it/get_it.dart';
-import 'package:go_router/go_router.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
@@ -21,47 +20,7 @@ class RegisterPage extends StatelessWidget {
           child: RegisterPageView(),
         ),
       ],
-      child: const RegisterPageView1(),
-    );
-  }
-}
-
-class RegisterPageView1 extends StatelessWidget {
-  const RegisterPageView1({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<RegisterCubit, RegisterState>(
-      builder: (context, state) {
-        return Column(
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                context.read<RegisterCubit>().registerUser(
-                  fullName: 'joao',
-                  email: 'dsadasadsddsas',
-                  password: '',
-                  birthDate: DateTime(
-                    2000,
-                    1,
-                    1,
-                  ), // Add birth date logic if needed
-                  phoneNumber: '', // Add phone number logic if needed
-                  zipCode: '', // Add zip code logic if needed
-                  address: '', // Add address logic if needed
-                  addressNumber: '', // Add address number logic if needed
-                  city: '', // Add city logic if needed
-                  state: '', // Add state logic if needed
-                  country: '', // Add country logic if needed
-                  termsAccepted: true,
-                  receivePromotions: true,
-                );
-              },
-              child: Text('Registrar'),
-            ),
-          ],
-        );
-      },
+      child: const RegisterPageView(),
     );
   }
 }
@@ -70,10 +29,10 @@ class RegisterPageView extends StatefulWidget {
   const RegisterPageView({super.key});
 
   @override
-  State<RegisterPageView> createState() => _RegisterPageViewState();
+  State<RegisterPageView> createState() => _RegisterPageView1State();
 }
 
-class _RegisterPageViewState extends State<RegisterPageView> {
+class _RegisterPageView1State extends State<RegisterPageView> {
   final _formKey = GlobalKey<FormState>();
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
@@ -89,32 +48,6 @@ class _RegisterPageViewState extends State<RegisterPageView> {
   bool _obscureText = true;
 
   DateTime? selectedDate;
-
-  void _register() {
-    if (_formKey.currentState!.validate()) {
-      GoRouter.of(context).go('/genie-slider');
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _dateFocus.addListener(() {
-      if (!_dateFocus.hasFocus) {
-        _formKey.currentState?.validate();
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    _nameController.dispose();
-    _emailController.dispose();
-    _passwordController.dispose();
-    _confirmPasswordController.dispose();
-    _dateFocus.dispose();
-    super.dispose();
-  }
 
   Future<void> _showDataPicker() async {
     showModalBottomSheet(
@@ -150,158 +83,172 @@ class _RegisterPageViewState extends State<RegisterPageView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Crie sua Conta e Entre na Ação!')),
-      body: SafeArea(
-        child: Form(
-          key: _formKey,
-          child: BlocBuilder<RegisterCubit, RegisterState>(
-            builder: (context, state) {
-              return ListView(
-                padding: const EdgeInsets.all(16.0),
-                shrinkWrap: true,
-                children: [
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.8,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        GlTextFormField(
-                          controller: _nameController,
-                          keyboardType: TextInputType.name,
-                          decoration: InputDecoration(
-                            labelText: 'Nome Completo',
-                          ),
-                          validator: (value) {
-                            return _validators.nameValidator(value);
-                          },
-                        ),
-                        GlTextFormField(
-                          controller: _emailController,
-                          keyboardType: TextInputType.emailAddress,
-                          decoration: InputDecoration(labelText: 'E-mail'),
-                          validator: (value) {
-                            return _validators.validateEmail(value);
-                          },
-                        ),
-                        GlTextFormField(
-                          controller: _passwordController,
-                          obscureText: _obscureText,
-                          autovalidateMode: AutovalidateMode.onUserInteraction,
-                          decoration: InputDecoration(
-                            labelText: 'Senha',
-                            suffixIcon: IconButton(
-                              icon: Icon(
-                                _obscureText
-                                    ? Icons.visibility
-                                    : Icons.visibility_off,
-                              ),
-                              onPressed:
-                                  () => setState(
-                                    () => _obscureText = !_obscureText,
-                                  ),
-                            ),
-                          ),
-                          validator: (value) {
-                            return _validators.validatePassword(value);
-                          },
-                        ),
-                        GlTextFormField(
-                          controller: _confirmPasswordController,
-                          obscureText: _obscureText,
-                          autovalidateMode: AutovalidateMode.onUserInteraction,
-                          decoration: InputDecoration(
-                            labelText: 'Confirme a Senha',
-                            suffixIcon: IconButton(
-                              icon: Icon(
-                                _obscureText
-                                    ? Icons.visibility
-                                    : Icons.visibility_off,
-                              ),
-                              onPressed:
-                                  () => setState(
-                                    () => _obscureText = !_obscureText,
-                                  ),
-                            ),
-                          ),
-                          validator: (value) {
-                            return _validators.validateConfirmPassword(
-                              value,
-                              _passwordController.text,
-                            );
-                          },
-                        ),
+    return BlocBuilder<RegisterCubit, RegisterState>(
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(title: Text('Crie sua Conta e Entre na Ação!')),
 
-                        GlTextFormField(
-                          keyboardType: TextInputType.datetime,
-                          inputFormatters: Formatters().dateFormatter,
-                          controller: _dateController,
-                          focusNode: _dateFocus,
-                          decoration: InputDecoration(
-                            labelText: 'Data de Nascimento (DD/MM/AAAA)',
-                          ),
-                          validator: (value) {
-                            return _validators.validateDate(value);
-                          },
-                          onTap: () => _showDataPicker(),
-                        ),
-                        GlTextFormField(
-                          keyboardType: TextInputType.phone,
-                          decoration: InputDecoration(
-                            labelText: 'Número de Telefone',
-                            prefixIcon: IconButton(
-                              icon: Icon(Icons.flag),
-                              onPressed: () {
-                                // Implementar lógica para selecionar a bandeira do país
-                              },
-                            ),
-                            prefixText:
-                                '+55 ', // Exemplo de prefixo para o Brasil
-                          ),
-                          validator: (value) {
-                            return _validators.validatePhoneNumber(value);
-                          },
-                        ),
-
-                        CheckboxListTile(
-                          title: Text('Aceitar Termos e Condições'),
-                          value: _acceptTerms,
-                          onChanged: (bool? value) {
-                            setState(() {
-                              _acceptTerms = value ?? false;
-                            });
-                          },
-                          controlAffinity: ListTileControlAffinity.leading,
-                        ),
-                        CheckboxListTile(
-                          title: Text('Receber Promoções'),
-                          value: _receivePromotions,
-                          onChanged: (bool? value) {
-                            setState(() {
-                              _receivePromotions = value ?? false;
-                            });
-                          },
-                          controlAffinity: ListTileControlAffinity.leading,
-                        ),
-                        Center(
-                          child: ElevatedButton(
-                            onPressed:
-                                (_formKey.currentState?.validate() ?? false) &&
-                                        _acceptTerms
-                                    ? _register
-                                    : null,
-                            child: Text('Registrar Agora'),
-                          ),
-                        ),
-                      ],
+          body: SafeArea(
+            child: BlocBuilder<RegisterCubit, RegisterState>(
+              builder: (context, state) {
+                if (state is RegisterLoadingState) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (state is RegisterErrorState) {
+                  return Center(
+                    child: Text(
+                      'Erro ao registrar: ${state.exception}',
+                      style: TextStyle(color: Colors.red),
                     ),
+                  );
+                } else if (state is RegisterSuccessState) {
+                  return Center(child: Text('Registro realizado com sucesso!'));
+                }
+                return Form(
+                  key: _formKey,
+                  child: ListView(
+                    padding: const EdgeInsets.all(16.0),
+                    shrinkWrap: true,
+                    children: [
+                      Column(
+                        spacing: 16,
+                        children: [
+                          GlTextFormField(
+                            controller: _nameController,
+                            keyboardType: TextInputType.name,
+                            decoration: InputDecoration(
+                              labelText: 'Nome Completo',
+                            ),
+                            validator: (value) {
+                              return _validators.nameValidator(value);
+                            },
+                          ),
+                          GlTextFormField(
+                            controller: _emailController,
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: InputDecoration(labelText: 'E-mail'),
+                            validator: (value) {
+                              return _validators.validateEmail(value);
+                            },
+                          ),
+                          GlTextFormField(
+                            controller: _passwordController,
+                            obscureText: _obscureText,
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            decoration: InputDecoration(labelText: 'Senha'),
+                            validator: (value) {
+                              return _validators.validatePassword(value);
+                            },
+                          ),
+                          GlTextFormField(
+                            controller: _confirmPasswordController,
+                            obscureText: _obscureText,
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            decoration: InputDecoration(
+                              labelText: 'Confirme a Senha',
+                            ),
+                            validator: (value) {
+                              return _validators.validateConfirmPassword(
+                                value,
+                                _passwordController.text,
+                              );
+                            },
+                          ),
+                          GlTextFormField(
+                            keyboardType: TextInputType.datetime,
+                            inputFormatters: Formatters().dateFormatter,
+                            controller: _dateController,
+                            focusNode: _dateFocus,
+                            decoration: InputDecoration(
+                              labelText: 'Data de Nascimento (DD/MM/AAAA)',
+                            ),
+                            validator: (value) {
+                              return _validators.validateDate(value);
+                            },
+                            onTap: () => _showDataPicker(),
+                          ),
+                          GlTextFormField(
+                            keyboardType: TextInputType.phone,
+                            decoration: InputDecoration(
+                              labelText: 'Número de Telefone',
+                              prefixIcon: IconButton(
+                                icon: Icon(Icons.phone),
+                                onPressed: () {
+                                  // Implementar lógica para selecionar a bandeira do país
+                                },
+                              ),
+                              prefixText:
+                                  '+55 ', // Exemplo de prefixo para o Brasil
+                            ),
+                            validator: (value) {
+                              return _validators.validatePhoneNumber(value);
+                            },
+                          ),
+                          CheckboxListTile(
+                            title: Text('Aceitar Termos e Condições'),
+                            value: _acceptTerms,
+                            onChanged: (bool? value) {
+                              setState(() {
+                                _acceptTerms = value ?? false;
+                              });
+                            },
+                            controlAffinity: ListTileControlAffinity.leading,
+                          ),
+                          CheckboxListTile(
+                            title: Text('Receber Promoções'),
+                            value: _receivePromotions,
+                            onChanged: (bool? value) {
+                              setState(() {
+                                _receivePromotions = value ?? false;
+                              });
+                            },
+                            controlAffinity: ListTileControlAffinity.leading,
+                          ),
+                          Center(
+                            child: ElevatedButton(
+                              onPressed:
+                                  (_formKey.currentState?.validate() ??
+                                              false) &&
+                                          _acceptTerms
+                                      ? () {
+                                        if (_formKey.currentState!.validate()) {
+                                          context
+                                              .read<RegisterCubit>()
+                                              .registerUser(
+                                                fullName: _nameController.text,
+                                                email: _emailController.text,
+                                                password:
+                                                    _passwordController.text,
+                                                birthDate: selectedDate!,
+                                                phoneNumber:
+                                                    _dateController.text,
+                                                zipCode: '00000-000',
+                                                address: 'Rua Exemplo',
+                                                addressNumber: '123',
+                                                city: 'São Paulo',
+                                                state: 'SP',
+                                                country: 'Brasil',
+                                                termsAccepted: _acceptTerms,
+                                                receivePromotions:
+                                                    _receivePromotions!,
+                                              );
+                                        }
+                                      }
+                                      : null,
+                              child: Text('Registrar Agora'),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                ],
-              );
-            },
+                );
+              },
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
