79438d23904823c527280b53f85e64e484fcda78
Alexandre Levi Miltião <ale.levi@hotmail.com>
2025-06-02 18:20:04 -0300

    Remove unused Firebase import from personal_info_page.dart

--- lib/modules/register/presenter/pages/personal_info_page.dart
+++ lib/modules/register/presenter/pages/personal_info_page.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:genie_luck/core/design/gl_primary_button.dart';
 import 'package:genie_luck/core/design/gl_text_form_field.dart';
